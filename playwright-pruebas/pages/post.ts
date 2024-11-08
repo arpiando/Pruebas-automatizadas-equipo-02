518508db12dd0aef950dce8a87a06ef772837425
fabriziocucina-uniandes <f.cucina@uniandes.edu.co>
2024-11-07 20:58:56 -0500

    reestrucutred the folder

---
+++ playwright-pruebas/pages/post.ts
@@ -0,0 +1,54 @@
+import { Page } from '@playwright/test';
+
+export class PostCreatePage {
+  private page: Page;
+  private postSelector: string = '.gh-nav-new-post';
+  private titleInput: string = '.gh-editor-title';
+  private contentInput: string = 'p[data-koenig-dnd-droppable="true"]';
+  private publishButton: string = '.gh-btn.gh-btn-editor.gh-publish-trigger';
+  private reviewButton: string = '[data-test-button="continue"]';
+  private confirmButton: string = '[data-test-button="confirm-publish"]';
+  private header: string = 'header.modal-header h1[data-test-complete-title] span';
+  private successMessage: string = 'Boom! It\'s out there.';
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async navigateToCreatePost(): Promise<void> {
+    await this.page.click(this.postSelector);
+  }
+
+  async createPost(title: string, content: string): Promise<void> {
+    await this.page.fill(this.titleInput, title);
+    await this.page.fill(this.contentInput, content);
+    
+    await this.page.click(this.publishButton);
+    await this.page.waitForSelector(this.reviewButton, { state: 'visible', timeout: 60000 });
+    await this.page.click(this.reviewButton);
+    
+    await this.page.waitForSelector(this.confirmButton, { state: 'attached', timeout: 60000 });
+    await this.page.waitForTimeout(500);
+    await this.page.waitForSelector(this.confirmButton, { state: 'visible', timeout: 60000 });
+
+    const buttonBoundingBox = await this.page.locator(this.confirmButton).boundingBox();
+    if (!buttonBoundingBox) {
+      throw new Error('Confirm button is not visible in the viewport');
+    }
+
+    const isElementClickable = await this.page.isVisible(this.confirmButton);
+    if (!isElementClickable) {
+      throw new Error('Confirm button is not clickable');
+    }
+    
+    await this.page.click(this.confirmButton, { timeout: 60000});
+  }
+
+  async isPostCreatedSuccessfully(): Promise<string | null> {
+    await this.page.waitForSelector(this.header, { state: 'attached' });
+    await this.page.waitForSelector(this.header, { state: 'visible' });
+    
+    const successText = await this.page.locator(this.header, { hasText: this.successMessage }).textContent();
+    return successText
+  }
+}
